0baa95ab69cb002edbaf20e62573cc8e7cb8a472
1992makram <makramkrayen@gmail.com>
2022-02-21 09:05:35 +0100

    commit

---
+++ src/component/profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Address() {
+  return (
+    <div>cite el manar benguerdane
+        
+    </div>
+  )
+}
+
+export default Address
---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import image from '../../img1.jpg';
+function ProfilePhoto() {
+  return (
+      <>
+  <img src={image}/>  
+  </>
+  )
+}
+
+export default ProfilePhoto
